9940bce465b356aa45caf701703fab8ff47ebd60
Alex Rostron <alex.rostron24@gmail.com>
2018-05-20 13:59:00 -0600

    Comment driver name

--- basic-servo.cpp
+++ basic-servo.cpp
@@ -1,5 +1,5 @@
 //from https://howtomechatronics.com/tutorials/arduino/how-to-control-stepper-motor-with-a4988-driver-and-arduino/
-
+// Stepper motor control using the A4988 Driver
 
 const int stepPin = 3;
 const int dirPin = 4;
