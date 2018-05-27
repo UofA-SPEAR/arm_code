3faeb5bce731632dafcae340b12902cdf4c54021
Alex Rostron <alex.rostron24@gmail.com>
2018-05-26 22:20:36 -0600

    Refactor main to a simple stepper motor test

--- main.cpp
+++ main.cpp
@@ -1,83 +1,36 @@
 #include <Arduino.h>
-#include <Stepper.h>
+#include <StepperC.h>
 #include <string.h>
-
-
-//Constants
-#define BASE 0
-#define SHOULDER 1
-#define ELBOW 2
-#define WRIST 3
-#define CLAW 5
-
-class Arm{
-	
-	public:
-		int arm_num, arm_step;
-		int stepsPerRevolution;
-		Arm();
-		void write_stepper(int stepper_num);
-	private:
-		Stepper base_stepper;
-		Stepper elbow_stepper;
-		Stepper shoulder_stepper;
-		Stepper wrist_stepper;
-
-};
-
-Arm::Arm():
-	stepsPerRevolution(64),
-	base_stepper(stepsPerRevolution,8,9,10,11){
-	base_stepper.setSpeed(600);	
-	elbow_stepper.setSpeed(600);
-	shoulder_stepper.setSpeed(600);
-	wrist_stepper.setSpeed(600);
-}
-
-void Arm::write_stepper(int stepper_num){
-	Serial.println(this->arm_step);
-	//TODO: change so that stepper is selected with stepper_num
-	this->base_stepper.step(this->arm_step);
-}
-
-Arm roverarm;
 
 void setup(){
 	init();
 	Serial.begin(9600);
 	Serial.println("Initalized");
-	
+
 }
 
 int main(){
-	//initalize connection with ui and ros
-	//run setup 
 	setup();
-	Serial.flush();
-	//initalize buffer for serial input
 
-	char buffer[129] = {0};
-	int used = 0;//keeps track of used buffer space
-	char inbyte;
-	while(true){
-		if(Serial.available() != 0){
-		//read ASCII int from serial
-		inbyte = Serial.read();
-	
-		//convert and add to buffer as character
-		buffer[used] = char(inbyte);
-		//add to buffer counter
-		used++;
+	Serial.println("init stepper");
+	int r = 200;
+	Stepper testMotor(r, 3, 4);
 
-		if(inbyte == '\n' | inbyte == '\r'){
-				
-			sscanf(buffer,"%i %i\n",&roverarm.arm_num,&roverarm.arm_step);
-			roverarm.write_stepper(roverarm.arm_num);
+	Serial.println("setting rotation speed");
+	testMotor.setSpeed(30);
 
-			Serial.flush();
-			}
-		}
-	}
+	Serial.println("stepping full rotation");
+	testMotor.step(r);
+
+	Serial.println("rotate to 1");
+	testMotor.rotateToRadian(1);
+
+	Serial.println("rotate to 2");
+	testMotor.rotateToRadian(2);
+
+	Serial.println("rotate to 1");
+	testMotor.rotateToRadian(1);
+
+
 	return 0;
 }
-
