58f64ca389d6599c3b1d3f2f0d648dbfe002f213
Alex Rostron <alex.rostron24@gmail.com>
2018-06-10 16:07:19 -0600

    Clean up comments

--- StepperC.cpp
+++ StepperC.cpp
@@ -1,5 +1,4 @@
-//copied from https://github.com/arduino/Arduino/blob/master/libraries/Stepper/src/Stepper.cpp
-//TODO: Modify for A4988 Driver compatability and radian mode
+//sourced from https://github.com/arduino/Arduino/blob/master/libraries/Stepper/src/Stepper.cpp
 
 #include "Arduino.h"
 #include "StepperC.h"
@@ -8,26 +7,22 @@
 
 Stepper::Stepper(int steps_per_rotation, int stepPin, int dirPin){
   /*
-  * two-wire constructor.
-  * Sets which wires should control the motor.
+  Constructor: 2-pin Stepper motor constructor with A4988 Driver module.
   */
-  this->step_number = 0;     // which step the motor is on
-  this->direction = HIGH;    // motor direction
-  this->last_step_time = 0;  // time stamp in us of the last step taken
-  this->steps_per_rotation = steps_per_rotation; // total number of steps for this motor
 
-  // Arduino pins for the motor control connection:
+  this->step_number = 0; // which step the motor is on
+  this->direction = HIGH;
+  this->last_step_time = 0;
+  this->steps_per_rotation = steps_per_rotation; //# of steps for 1 rotation
   this->stepPin = stepPin;
   this->dirPin = dirPin;
-
-  // setup the pins on the microcontroller:
-  pinMode(this->stepPin, OUTPUT);
-  pinMode(this->dirPin, OUTPUT);
-  digitalWrite(this->dirPin, this->direction); //MIGHT NOT BE FORMATTED CORRECT?
-
-  // Radian related constants
   this->radian_per_step = (2 * PI) / steps_per_rotation;
   this->current_motor_radian = 0; //How do you know position 0?
+
+  //setup the pins on the microcontroller:
+  pinMode(this->stepPin, OUTPUT);
+  pinMode(this->dirPin, OUTPUT);
+  digitalWrite(this->dirPin, this->direction);
 }
 
 
@@ -35,7 +30,7 @@ void Stepper::rotateToRadian(float target_radian){
   /*
   given target radian rotate to that value using a calculated number of steps
   */
-  //target_radian = target_radian % (2*PI);
+  //Still need to account for bounds
   float diff = (target_radian - this->current_motor_radian);
   if(diff > PI){
       diff = diff - 2*PI;
@@ -45,7 +40,6 @@ void Stepper::rotateToRadian(float target_radian){
 
   }
   int required_steps = diff * (1/radian_per_step);
-  //Serial.println("Current: "+this->current_motor_radian+" required_steps: "+required_steps+" Target: "+target_radian);
   step(required_steps);
   this->current_motor_radian = fmod(target_radian, (2*PI)); //hopefully
 }
@@ -53,15 +47,15 @@ void Stepper::rotateToRadian(float target_radian){
 
 void Stepper::setSpeed(long RPM){
   /*
-   * Sets the speed in revs per minute
-   */
+  Sets the speed in revs per minute
+  */
   this->step_delay = 60L * 1000L * 1000L / this->steps_per_rotation / RPM;
 }
 
 
 void Stepper::step(int steps_to_move){
   /*
-   * Moves the motor steps_to_move steps.  If the number is negative,
+   * Moves the motor [steps_to_move] steps.  If the number is negative,
    * the motor moves in the reverse direction.
    */
   int steps_left = abs(steps_to_move);  // how many steps to take
@@ -76,10 +70,9 @@ void Stepper::step(int steps_to_move){
     digitalWrite(this->dirPin, direction);
    }
 
-
   // decrement the number of steps, moving one step each time:
   while (steps_left > 0){
-    unsigned long now = micros(); // MIGHT >1 hour running problem??
+    unsigned long now = micros(); // MIGHT >1 hour running problem?
     // move only if the appropriate delay has passed:
     if (now - this->last_step_time >= this->step_delay){
       // get the timeStamp of when you stepped:
@@ -99,10 +92,8 @@ void Stepper::step(int steps_to_move){
       }
       // decrement the steps left:
       steps_left--;
-
       //Cause pulse of stepper motor
-      //intended runtime of stepMotor is one step_delay
-      stepMotor((unsigned long)(this->step_delay / 2));  //TIME NOT CORRECT?
+      stepMotor((unsigned long)(this->step_delay / 2));
     }
   }
 }
@@ -110,10 +101,9 @@ void Stepper::step(int steps_to_move){
 
 void Stepper::stepMotor(int rotationDelay){
   /*
-  Steps the motor by a single step
-  rotationDelay [300,4000] from example
+  Steps the motor by a single step, runtime: 2*rotationDelay
   */
-  digitalWrite(stepPin,HIGH); //apparently 2 microseconds?
+  digitalWrite(stepPin,HIGH);
   delayMicroseconds(rotationDelay);
   digitalWrite(stepPin,LOW);
   delayMicroseconds(rotationDelay);
