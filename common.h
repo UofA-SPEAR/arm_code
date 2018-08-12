e30903b7e81d2b86a177745778247a999553bc1f
Jacob Reckhard <jacobreckhard@gmail.com>
2018-08-11 22:46:07 -0600

    defined more ints

--- common.h
+++ common.h
@@ -6,4 +6,21 @@
 #ifndef UINT32_MAX
 #define UINT32_MAX 4294967295
 #endif
+
+#ifndef INT32_MAX
+#define INT32_MAX 2147483647
+#endif
+
+#ifndef INT32_MIN
+#define INT32_MIN -2147483648
+#endif
+
+#ifndef INT16_MAX
+#define INT16_MAX 32767
+#endif
+
+#ifndef INT16_MIN
+#define INT16_MIN -32768
+#endif
+
 #endif
